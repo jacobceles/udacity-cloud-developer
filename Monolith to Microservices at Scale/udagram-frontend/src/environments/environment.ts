8eae8333561f1cd3e37cb5808a2d52ae702f6fd8
Jacob Celestine <jacobceles@gmail.com>
2024-07-14 17:12:30 -0400

    updated config and frontend app

--- Monolith to Microservices at Scale/udagram-frontend/src/environments/environment.ts
+++ Monolith to Microservices at Scale/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a59455ab0f6ff4ec9a2869f2f9c7b346-1524277599.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
